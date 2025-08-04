914640631e0ddb3c6d936a15f9689e0ce73d3bf1
Alexandre Agustini <alexandre.agustini@pucrs.br>
2025-08-04 14:09:50 +0000

    Exercicio Circulo

--- CirculosPrj/src/main/java/edu/aagustini/App.java
+++ CirculosPrj/src/main/java/edu/aagustini/App.java
@@ -2,6 +2,22 @@ package edu.aagustini;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("\n-\n-\n-Hello, world! Projeto CirculosPrj criado com sucesso.");
+        Circulo c1 = new Circulo();
+        System.out.println(c1.toString());
+
+       
+        
+        System.out.println("area do c1: " + c1.area());
+
+
+        Circulo c2 = new Circulo(3,4,5);
+        System.out.println(c2.toString());
+
+        c2.mover(1,3);
+        System.out.println(c2.toString());
+
+        c2.zoom(0.5);
+        System.out.println(c2.toString());
+
     }
 }
