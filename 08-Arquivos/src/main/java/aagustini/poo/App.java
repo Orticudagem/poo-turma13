b468931f2f018e1eb9b3762fc117b740386161c6
Alexandre Agustini <alexandre.agustini@pucrs.br>
2025-09-14 19:25:59 +0000

    atualizado o readme

--- 08-Arquivos/src/main/java/aagustini/poo/App.java
+++ 08-Arquivos/src/main/java/aagustini/poo/App.java
@@ -1,5 +1,8 @@
 package aagustini.poo;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class App {
 
     public static void main(String[] args) {
@@ -20,8 +23,23 @@ public class App {
         cad.readFile("funcionarios.txt");
 
         System.out.println(cad.toString());
-
+        System.out.println("\n");
         System.out.println(cad.relatorio());
-
+        System.out.println("\n");
+        //teste();
     }
+
+ 
+    public static void teste() {
+
+        Path path1 = Paths.get("/workspaces/poo-turma13/08-Arquivos/funcionarios.txt");
+
+        System.out.println("toString: "     + path1.toString());     // /home/user2/statusReport
+        System.out.println("getFileName: "  + path1.getFileName());  // statusReport
+        System.out.println("getName(0): "   + path1.getName(0));     // home
+        System.out.println("getNameCount: " + path1.getNameCount()); // 3
+        System.out.println("subpath(0,2): " + path1.subpath(0,2));   // /home/user2
+        System.out.println("getParent: "    + path1.getParent());    // /home/user2
+        System.out.println("getRoot: "      + path1.getRoot());      // / ou C:\
+    }   
 }
