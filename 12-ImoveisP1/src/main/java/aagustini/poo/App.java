5f29b0ba2f1ee0431f2159f6fd2bc9193de9c837
Alexandre Agustini <alexandre.agustini@pucrs.br>
2025-09-22 13:43:20 +0000

    Finalização do código da prova P1

--- 12-ImoveisP1/src/main/java/aagustini/poo/App.java
+++ 12-ImoveisP1/src/main/java/aagustini/poo/App.java
@@ -2,6 +2,38 @@ package aagustini.poo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world! Projeto 12-ImoveisP1 criado com sucesso.");
+        
+        Imovel.setVR(10);
+
+        // imovel é abstract não pode ser instanciada
+        //Imovel i0 = new Imovel("alex", 
+        //                            200, 
+        //                            new Geo(10,20));
+
+        Imovel i1 = new Residencial("alex", 
+                                    200, 
+                                    new Geo(10,20),
+                                    1100);
+        Imovel i2 = new Residencial("hugo", 
+                                    310, 
+                                    new Geo(10,20),
+                                    700);
+        Imovel i3 = new Comercial("zeca",    200, 
+                                    new Geo(10,20));
+        Imovel i4 = new Comercial("hugo",    200, 
+                                    new Geo(10,20));                           
+
+        CadastroImoveis cadastro = new CadastroImoveis();
+        cadastro.cadastrar(i1);
+        cadastro.cadastrar(i3);
+        cadastro.cadastrar(i4);
+        cadastro.cadastrar(i2);
+
+        cadastro.listar();
+
+        cadastro.listar("hugo");
+        
+
+    
     }
 }
